26ce4bc728b5a179a26c6e189ee1df0110482ecd
Kaidoisj <108651851+Kaidoisj@users.noreply.github.com>
2022-07-14 16:53:17 +0800

    update

--- Update.Script.lua
+++ Update.Script.lua
@@ -3,5 +3,5 @@ local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xNelzbutDiffffx/main/NelzHub.LuaScript123",true))()
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xUpdate/main/xluax",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xUpdate/main/xluax",true))()
 end

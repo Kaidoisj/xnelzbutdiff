06626c20f3fa0d62687b046ffdc6d97e77eb4983
Kaidoisj <108651851+Kaidoisj@users.noreply.github.com>
2022-07-14 15:38:50 +0800

    Update All.Script.lua

--- All.Script.lua
+++ All.Script.lua
@@ -4,7 +4,7 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xNelzbutDiffffx/main/NelzHub.LuaScript123",true))()
 end 
    print 'bobo ka tanginamo'
-end
+
 game:GetService("StarterGui"):SetCore("SendNotification",{     
 Title = "Nelz Hub",     
 Text = "Thanks for Using Nelz hub giatay ka",

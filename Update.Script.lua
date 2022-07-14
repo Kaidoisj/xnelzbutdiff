7a374fcb0a78dbda9815088ba9de95cec4550ede
Kaidoisj <108651851+Kaidoisj@users.noreply.github.com>
2022-07-14 16:52:13 +0800

    update

--- Update.Script.lua
+++ Update.Script.lua
@@ -2,5 +2,6 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xNelzbutDiffffx/main/NelzHub.LuaScript123",true))()
-end 
-   print 'bobo ka tanginamo'
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidoisj/xUpdate/main/xluax",true))()
+end
